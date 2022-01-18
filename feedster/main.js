8d638fc81d6a8f70110bc6c48e468fd6eecd2315
LAPTOP-1A0N2URC\Bolejko <jaroslawbolejko85@gmail.com>
2022-01-18 13:56:36 +0100

    add keyup to change msg color

--- feedster/main.js
+++ feedster/main.js
@@ -5,7 +5,23 @@ $('.menu').on('mouseenter',() => {
 
  $('.btn').on('mouseenter', (event) => {
     $(event.currentTarget).addClass('btn-hover').on('mouseleave', (event) => {
-       $(event.currentTarget).removeClass('btn-hover')});
+       $(event.currentTarget).removeClass('btn-hover');
     });
+});
+
+$('.postText').on('keyup', (event)=>{
+   let post = $(event.currentTarget).val();
+   let remaining = 140 - post.length; 
+
+   if(remaining <= 0) {
+       $('.wordcount').addClass('red');
+   }
+   else{
+    $('.wordcount').removeClass('red');  
+   }
+   
+   $('.characters').html(remaining);
+});
+$('.postText').focus();
 
 }); 
