b7795aa4c9c7034daaeb9ae8ae90bee861e55ad1
LAPTOP-1A0N2URC\Bolejko <jaroslawbolejko85@gmail.com>
2022-01-19 22:08:53 +0100

    add my code to script.js

--- Gameboard/script.js
+++ Gameboard/script.js
@@ -1,5 +1,16 @@
 var main = function() {
 
+$('.more-btn').on('click', event => {
+    $(event.currentTarget).siblings('.more-menu').toggle();
+});
+$('.share').on('click', event => {
+    $(event.currentTarget).next('.share-menu').toggle();
+});
+$('.notification').on('click', event =>{
+    $(event.currentTarget).toggleClass('active');
+})
+
+
 };
 
 $(document).ready(main);
