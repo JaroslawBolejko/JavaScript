24af26495630566dd80f44d604d65fc7471df0b7
LAPTOP-1A0N2URC\Bolejko <jaroslawbolejko85@gmail.com>
2022-01-19 12:29:58 +0100

    add jquery code to main.js

--- WebSafeFontPreview/main.js
+++ WebSafeFontPreview/main.js
@@ -1,3 +1,18 @@
 $(document).ready(() => {
-  
-})
+  $('#text').on('keyup', event => {
+    let currentElement = $(event.currentTarget).val();
+    $('.preview').html(currentElement);
+  });
+
+  $('#font').on('change', event => {
+      $('.preview').css('fontFamily', $(event.currentTarget).val())
+  });
+  $('#weight').on('change', event => {
+    $('.preview').css('fontWeight', $(event.currentTarget).val())
+});
+$('#size').on('keyup', event => {
+    let fontSize = $(event.currentTarget).val() + 'px';
+    $('.preview').css('fontSize', fontSize);
+  });
+
+});
