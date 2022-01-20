60957c6c5653c218c27bf8bfffe59c9354aa5ca3
LAPTOP-1A0N2URC\Bolejko <jaroslawbolejko85@gmail.com>
2022-01-20 23:02:00 +0100

    add main.js content

--- MessageGenerator/README.md
+++ MessageGenerator/README.md
@@ -1,1 +1,1 @@
-Random message generator.Every time a user runs a program, he gets a new, randomized output.
+Random 'Yo mama' mix message generator.Every time a user runs a program, he gets a new, randomized output. Sometimes without sense. Project made form scratch without any given files.
---
+++ MessageGenerator/main.js
@@ -0,0 +1,20 @@
+const  isSoArray = ['so hot', 'so finger lickin good', 'so fine', 'so sweet'];
+
+const thatArray = ['she\'s banned from National parks', 'she should sell hotdogs', 'she doesn\'t need any sugar in her coffee',
+ 'last name must be pepper'];
+
+const whyArray = ['because she\'s the leading cause of wildfires','because she already knows how to make a weiner stand', 
+'cause she\'s got a pretty sweet but','cause she\'s SMOKIN!'];
+
+function randomIndex(array) {
+   return Math.floor(Math.random() * array.length);
+ }
+
+function mixMessage() {
+
+return `Yo mama ${isSoArray[randomIndex(isSoArray)]} ${thatArray[randomIndex(thatArray)]} ${whyArray[randomIndex(whyArray)]}`;
+
+};
+
+
+console.log(mixMessage())
