71cba7946a33bce95e8a5e8e34ce2eed6dbf15b7
LAPTOP-1A0N2URC\Bolejko <jaroslawbolejko85@gmail.com>
2022-01-21 10:09:17 +0100

    add main.js content

--- MessageGenerator/main.js
+++ MessageGenerator/main.js
@@ -3,18 +3,22 @@ const  isSoArray = ['so hot', 'so finger lickin good', 'so fine', 'so sweet'];
 const thatArray = ['she\'s banned from National parks', 'she should sell hotdogs', 'she doesn\'t need any sugar in her coffee',
  'last name must be pepper'];
 
-const whyArray = ['because she\'s the leading cause of wildfires','because she already knows how to make a weiner stand', 
-'cause she\'s got a pretty sweet but','cause she\'s SMOKIN!'];
+const whyArray = ['because she\'s the leading cause of wildfires.','because she already knows how to make a weiner stand.', 
+'cause she\'s got a pretty sweet but.','cause she\'s SMOKIN!.'];
 
 function randomIndex(array) {
    return Math.floor(Math.random() * array.length);
  }
 
-function mixMessage() {
+function message () {
 
-return `Yo mama ${isSoArray[randomIndex(isSoArray)]} ${thatArray[randomIndex(thatArray)]} ${whyArray[randomIndex(whyArray)]}`;
+    return `Yo mama ${isSoArray[randomIndex(isSoArray)]} ${thatArray[randomIndex(thatArray)]} ${whyArray[randomIndex(whyArray)]}`;
 
 };
 
-
-console.log(mixMessage())
+let mixMessage = message();
+$(document).ready(() => {
+  $('.btn').on('click',() => {
+        $('#preview').html(mixMessage);
+ });
+});
